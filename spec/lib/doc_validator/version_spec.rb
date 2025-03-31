d03d0dce02241a1bf0589737120a344a2a2d1a41
Maikon Douglas <maikonconta0@gmail.com>
2025-03-31 00:00:25 -0300

    update version

--- spec/lib/doc_validator/version_spec.rb
+++ spec/lib/doc_validator/version_spec.rb
@@ -6,7 +6,7 @@ require 'spec_helper'
 RSpec.describe DocValidator do
   describe 'VERSION' do
     it 'has a version number' do
-      expect(DocValidator::VERSION).to eq('1.2.6')
+      expect(DocValidator::VERSION).to eq('1.2.7')
     end
   end
 end
